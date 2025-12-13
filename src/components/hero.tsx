1c4e955d43a08d0cad4b5766534a80b1e60adde3
pinkkila <petteri.pinkkila@outlook.com>
2025-12-13 16:16:23 +0200

    refactor hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,4 @@
-import {TextEffect} from '@/components/ui/text-effect'
-
+import { TextEffect } from "@/components/ui/text-effect";
 
 export default function Hero() {
   return (
@@ -7,13 +6,11 @@ export default function Hero() {
       <main className="overflow-hidden">
         <div
           aria-hidden
-          className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block">
-          <div
-            className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]"/>
-          <div
-            className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]"/>
-          <div
-            className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]"/>
+          className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block"
+        >
+          <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
+          <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
+          <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
         </div>
         <section>
           <div className="relative pt-24 md:pt-36">
@@ -24,13 +21,13 @@ export default function Hero() {
 
             <div className="mx-auto max-w-7xl px-6">
               <div className="text-center sm:mx-auto lg:mr-auto lg:mt-0 mb-8">
-
                 <TextEffect
                   preset="fade-in-blur"
                   speedSegment={0.3}
                   as="h1"
-                  className="mx-auto mt-8 max-w-4xl text-balance text-5xl max-md:font-semibold md:text-7xl lg:mt-16 xl:text-[5.25rem]">
-                  Welcome to my Portfolio
+                  className="mx-auto mt-8 max-w-4xl text-balance text-5xl max-md:font-semibold md:text-7xl lg:mt-16 xl:text-[5.25rem]"
+                >
+                  Petteri Pinkkilä
                 </TextEffect>
                 <TextEffect
                   per="line"
@@ -38,15 +35,27 @@ export default function Hero() {
                   speedSegment={0.3}
                   delay={0.5}
                   as="p"
-                  className="mx-auto mt-8 max-w-2xl text-balance text-lg">
-                  I&#39;m Petteri Pinkkilä and this is my portfolio.
+                  className="mx-auto mt-8 max-w-2xl text-balance text-lg sm:text-xl"
+                >
+                  Hey, I&#39;m Petteri and I'm software development student at
+                  Haaga-Helia University of Applied Sciences, specializing in
+                  full-stack web development.
                 </TextEffect>
-
+                <TextEffect
+                  per="line"
+                  preset="fade-in-blur"
+                  speedSegment={0.3}
+                  delay={0.5}
+                  as="p"
+                  className="mx-auto mt-8 max-w-2xl text-balance text-lg sm:text-xl"
+                >
+                  kdkdkd
+                </TextEffect>
               </div>
             </div>
           </div>
         </section>
       </main>
     </>
-  )
+  );
 }
