521d022d2a13845ce177b5b71dadfd65bb34ba1b
pinkkila <petteri.pinkkila@outlook.com>
2025-12-17 18:29:50 +0200

    add different button style to header when mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -74,12 +74,36 @@ export const Header = () => {
                   ))}
                 </ul>
               </div>
-              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+              <div className="flex w-full flex-col space-y-6 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+                <Button
+                  asChild
+                  variant="outline"
+                  size="lg"
+                  className="sm:hidden"
+                >
+                  <a
+                    href="https://www.linkedin.com/in/pinkkila/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="LinkedIn"
+                    className="text-primary block">
+                    <svg
+                      className="size-6 scale-[1.28]"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"></path>
+                    </svg>
+                  </a>
+                </Button>
                 <Button
                   asChild
                   variant="link"
                   size="sm"
-                  className="text-muted-foreground hover:text-primary"
+                  className="text-muted-foreground hover:text-primary hidden sm:flex sm:items-center"
                 >
                   <a
                     href="https://www.linkedin.com/in/pinkkila/"
@@ -99,11 +123,37 @@ export const Header = () => {
                     </svg>
                   </a>
                 </Button>
+
+
+                <Button
+                  asChild
+                  variant="outline"
+                  size="lg"
+                  className="sm:hidden"
+                >
+                  <a
+                    href="https://github.com/pinkkila"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="GitHub"
+                    className="text-primary block">
+                    <svg
+                      className="size-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="1em"
+                      height="1em"
+                      viewBox="0 0 98 96">
+                      <path
+                        fill="currentColor"
+                        d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"></path>
+                    </svg>
+                  </a>
+                </Button>
                 <Button
                   asChild
                   variant="link"
                   size="sm"
-                  className="text-muted-foreground hover:text-primary"
+                  className="text-muted-foreground hover:text-primary hidden sm:flex sm:items-center"
                 >
                   <a
                     href="https://github.com/pinkkila"
